8c1017a61ea9bd120d95f008185aeb928d96d6d3
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-29 21:00:11 -0700

    Add multi-colored buttons to play different sounds

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,81 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
+                  playSound(1);
                 },
-                child: Text('Click Me')),
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+                child: null,
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+//Center(
+//child: FlatButton(
+//onPressed: () {
+//final player = AudioCache();
+//player.play('note1.wav');
+//},
+//child: Text('Click Me')),
+//),
